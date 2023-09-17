29dc65461e7244a8843e1744e7c656d7dd21426b
elkalytics <napoleonic_bores@proton.me>
2023-09-17 18:40:00 -0500

    Updated
    Added the facet_order parameter to control the order of facets (ascending or descending).
    Modified the synthetic data generation to include an additional category ('D').
    Improved the plot titles for better clarity in both interactive and non-interactive versions.
    Enhanced facet grid appearance in the non-interactive version.

--- templates/create_histogram.py
+++ templates/create_histogram.py
@@ -1,48 +1,58 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
+import matplotlib.pyplot as plt
 
-def create_histogram(data, variable, binwidth=None, interactive=False, aesthetics=None, facet_by=None, title=None):
+def create_histogram(data, variable, binwidth=None, interactive=False, aesthetics=None, facet_by=None, title=None, facet_order='ascending'):
     """
     Create a histogram.
 
     Parameters:
-    data (pd.DataFrame): The data to plot
-    variable (str): The variable to plot
-    binwidth (float, optional): Binwidth; computes optimally if not provided
-    interactive (bool, optional): Use interactive plotly plot if True, else matplotlib plot
-    aesthetics (dict, optional): Additional aesthetic parameters
-    facet_by (str, optional): Column name to facet by
-    title (str, optional): Plot title
+    data (pd.DataFrame): The data to plot.
+    variable (str): The variable to plot.
+    binwidth (float, optional): Binwidth; if not provided, it will be computed using Freedman-Diaconis rule.
+    interactive (bool, optional): Use interactive plotly plot if True, else seaborn plot. Default is False.
+    aesthetics (dict, optional): Additional aesthetic parameters for the seaborn plot.
+    facet_by (str, optional): Column name to facet by.
+    title (str, optional): Plot title.
+    facet_order (str, optional): Order in which to display the facets ('ascending', 'descending'). Default is 'ascending'.
 
     Returns:
     None
     """
-
+    
     aesthetics = aesthetics or {}
 
-    # Compute optimal binwidth using Freedman-Diaconis rule if not provided
     if binwidth is None:
         iqr = np.subtract(*np.percentile(data[variable], [75, 25]))
-        binwidth = 2 * iqr * (len(data[variable]) ** (-1/3))
+        binwidth = 2 * iqr / (len(data[variable]) ** (1/3))
+
+    if facet_order not in ['ascending', 'descending']:
+        raise ValueError("Invalid facet_order. Choose from 'ascending' or 'descending'.")
+
+    category_order = sorted(data[facet_by].unique(), reverse=(facet_order=='descending'))
 
     if interactive:
-        aesthetics.setdefault("color", facet_by)
-        fig = px.histogram(data, x=variable, nbins=int(binwidth), facet_col=facet_by, title=title, **aesthetics)
+        fig = px.histogram(data, x=variable, nbins=int(binwidth), facet_row=facet_by, 
+                           category_orders={facet_by: category_order}, 
+                           title=title, **aesthetics)
+        fig.update_layout(bargap=0.1)
+        fig.update_yaxes(title_text='Frequency')
+        fig.update_xaxes(title_text=variable)
         fig.show()
     else:
-        aesthetics.setdefault("color", "blue")
-        fig, ax = plt.subplots()
+        g = sns.FacetGrid(data, col=facet_by, sharey=False, col_wrap=2, col_order=category_order)
+        g.map(sns.histplot, variable, binwidth=binwidth, **aesthetics)
         
-        if facet_by:
-            data.groupby(facet_by)[variable].apply(lambda x: x.hist(bins=int(binwidth), ax=ax, **aesthetics))
-        else:
-            data[variable].hist(bins=int(binwidth), ax=ax, **aesthetics)
-        
+        # Adding labels and titles
+        g.set_axis_labels(variable, "Frequency")
+        g.set_titles(col_template="{col_name}", row_template="{row_name}")
+
         if title:
-            ax.set_title(title)
-        
+            plt.subplots_adjust(top=0.9)
+            g.fig.suptitle(title)
+
         plt.show()
 
 # Creating synthetic data
@@ -51,8 +61,8 @@ data_size = 1000
 data = pd.DataFrame({
     'variable1': np.random.randn(data_size),
     'variable2': np.random.rand(data_size),
-    'category': np.random.choice(['A', 'B', 'C'], size=data_size)
+    'category': np.random.choice(['A', 'B', 'C', 'D'], size=data_size)
 })
 
 # Usage
-create_histogram(data, 'variable1', interactive=True, facet_by='category', title="Interactive Histogram")
+create_histogram(data, 'variable1', interactive=True, facet_by='category', title="Histogram Analysis by Category", facet_order='descending')
