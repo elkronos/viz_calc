b43e4dfc1886c4bffeb43b9c5529720f27522e46
elkalytics <napoleonic_bores@proton.me>
2023-09-17 23:08:29 -0500

    Updates
    Import Statement: An import statement was added to include Optional from the typing module.
    
    Function Signature:
    
    Three new parameters were added:
    save_as (default value: None) allows saving the plot as a file.
    decimals (default value: 2) controls the number of decimal places to be displayed in the correlation annotations.
    triangle (default value: 'full') allows displaying either the full, lower, or upper triangle of the heatmap.
    Docstring:
    
    Descriptions for the new parameters (save_as, decimals, and triangle) were added to the docstring.
    The raises section in the docstring was updated to include error handling for the new triangle parameter.
    Error Handling:
    
    Added a new condition to check the validity of the triangle parameter, raising a ValueError for invalid inputs.
    Annotation Decimal Place:
    
    The number of decimal places in annotations was modified to use the value from the new decimals parameter instead of a hardcoded value of 2.
    Heatmap Display:
    
    Introduced conditional logic to create a mask depending on the triangle parameter value. This mask helps in displaying either the full, lower, or upper triangle of the correlation matrix.
    Changed the heatmap plotting section to include the mask parameter, and to return a matplotlib.axes._axes.Axes object. Also, set the title using ax.set_title instead of using set_title method directly on the heatmap function.
    Save Plot:
    
    A new condition was introduced to save the plot as a file if a filename is provided through the save_as parameter.
    Function Call:
    
    Modified the function call at the end to include the triangle parameter with the value 'lower', so only the lower triangle of the heatmap will be displayed.

--- analyses/correlogram.py
+++ analyses/correlogram.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.stats import spearmanr, pearsonr
+from typing import Optional
 
-def correlogram(data: pd.DataFrame, method: str = 'pearson', color: str = 'coolwarm', annotate: bool = False, significance: bool = False, p_threshold: float = 0.05, figsize: tuple = (10, 8), title: str = "Correlation Heatmap") -> None:
+def correlogram(data: pd.DataFrame, method: str = 'pearson', color: str = 'coolwarm', annotate: bool = False, significance: bool = False, p_threshold: float = 0.05, figsize: tuple = (10, 8), title: str = "Correlation Heatmap", save_as: Optional[str] = None, decimals: int = 2, triangle: str = 'full') -> None:
     """
     Computes a correlation heatmap with options for Pearson's or Spearman's correlations.
 
@@ -15,11 +16,17 @@ def correlogram(data: pd.DataFrame, method: str = 'pearson', color: str = 'coolw
     :param p_threshold: The p-value threshold for significance. Default is 0.05.
     :param figsize: The size of the heatmap figure. Default is (10, 8).
     :param title: The title of the heatmap. Default is "Correlation Heatmap".
+    :param save_as: The name of the file to save the heatmap as. If None, the plot will not be saved. Default is None.
+    :param decimals: The number of decimal places to use for the correlation values in annotations. Default is 2.
+    :param triangle: Which part of the triangle to display ('full', 'lower', 'upper'). Default is 'full'.
 
-    :raises ValueError: If an invalid method is provided.
+    :raises ValueError: If an invalid method or triangle parameter is provided.
     """
     if method not in ['pearson', 'spearman']:
         raise ValueError("Invalid method. Use 'pearson' or 'spearman'.")
+
+    if triangle not in ['full', 'lower', 'upper']:
+        raise ValueError("Invalid triangle parameter. Use 'full', 'lower', or 'upper'.")
 
     if method == 'pearson':
         corr_func = pearsonr
@@ -30,15 +37,26 @@ def correlogram(data: pd.DataFrame, method: str = 'pearson', color: str = 'coolw
     p_vals = data.corr(method=lambda x, y: corr_func(x, y)[1], numeric_only=True)
 
     if annotate:
-        annotations = corr.round(2).astype(str)
+        annotations = corr.round(decimals).astype(str)
         if significance:
             significant = p_vals < p_threshold
             annotations[significant] = annotations[significant] + '*'
     else:
         annotations = None
 
+    if triangle == 'lower':
+        mask = np.triu(np.ones_like(corr, dtype=bool))
+    elif triangle == 'upper':
+        mask = np.tril(np.ones_like(corr, dtype=bool))
+    else:
+        mask = None
+
     sns.set(rc={'figure.figsize': figsize})
-    sns.heatmap(corr, annot=annotations, cmap=color, fmt='', vmin=-1, vmax=1).set_title(title)
+    ax = sns.heatmap(corr, annot=annotations, cmap=color, fmt='', vmin=-1, vmax=1, mask=mask)
+    ax.set_title(title)
+
+    if save_as:
+        ax.get_figure().savefig(save_as)
 
 # Generate a synthetic dataset
 np.random.seed(0)
@@ -59,4 +77,4 @@ data = {
 df = pd.DataFrame(data)
 
 # Use the function to compute and plot the heatmap
-correlogram(df, method='spearman', annotate=True, significance=True)
+correlogram(df, method='spearman', annotate=True, significance=True, triangle = 'lower')
